c51cb01ba5958ffa69f35626b9cbc847833cef19
Kaichi-Irie <jjsnoseitodesu@gmail.com>
2025-03-19 08:00:19 +0900

    :zap: Add unit tests for TranslatePushPop function in CodeWriter

---
+++ vm/vmtranslator/codewriter_test.go
@@ -0,0 +1,52 @@
+package vmtranslator
+
+import (
+	"testing"
+)
+
+func TestTranslatePushPop(t *testing.T) {
+
+	tests := []struct {
+		ctype    VMCommandType
+		seg      string
+		idx      int
+		fileName string
+		want     string
+	}{
+		{C_PUSH, "local", 3, "Test", "@LCL\nD=M\n@3\nA=D+A\nD=M\n@SP\nA=M\nM=D\n@SP\nM=M+1\n"},
+		{C_POP, "local", 3, "Test", "@LCL\nD=M\n@3\nD=D+A\n@R13\nM=D\n@SP\nM=M-1\nA=M\nD=M\n@R13\nA=M\nM=D\n"},
+		{C_PUSH, "argument", 2, "Test", "@ARG\nD=M\n@2\nA=D+A\nD=M\n@SP\nA=M\nM=D\n@SP\nM=M+1\n"},
+		{C_POP, "argument", 2, "Test", "@ARG\nD=M\n@2\nD=D+A\n@R13\nM=D\n@SP\nM=M-1\nA=M\nD=M\n@R13\nA=M\nM=D\n"},
+		{C_PUSH, "this", 3, "Test", "@THIS\nD=M\n@3\nA=D+A\nD=M\n@SP\nA=M\nM=D\n@SP\nM=M+1\n"},
+		{C_POP, "this", 3, "Test", "@THIS\nD=M\n@3\nD=D+A\n@R13\nM=D\n@SP\nM=M-1\nA=M\nD=M\n@R13\nA=M\nM=D\n"},
+		{C_PUSH, "that", 3, "Test", "@THAT\nD=M\n@3\nA=D+A\nD=M\n@SP\nA=M\nM=D\n@SP\nM=M+1\n"},
+		{C_POP, "that", 3, "Test", "@THAT\nD=M\n@3\nD=D+A\n@R13\nM=D\n@SP\nM=M-1\nA=M\nD=M\n@R13\nA=M\nM=D\n"},
+		{C_PUSH, "temp", 7, "Test", "@R5\nD=M\n@7\nA=D+A\nD=M\n@SP\nA=M\nM=D\n@SP\nM=M+1\n"},
+		{C_POP, "temp", 7, "Test", "@R5\nD=M\n@7\nD=D+A\n@R13\nM=D\n@SP\nM=M-1\nA=M\nD=M\n@R13\nA=M\nM=D\n"},
+		{C_PUSH, "pointer", 0, "Test", "@THIS\nD=M\n@0\nA=D+A\nD=M\n@SP\nA=M\nM=D\n@SP\nM=M+1\n"},
+		{C_POP, "pointer", 0, "Test", "@THIS\nD=M\n@0\nD=D+A\n@R13\nM=D\n@SP\nM=M-1\nA=M\nD=M\n@R13\nA=M\nM=D\n"},
+		{C_PUSH, "pointer", 1, "Test", "@THAT\nD=M\n@1\nA=D+A\nD=M\n@SP\nA=M\nM=D\n@SP\nM=M+1\n"},
+		{C_POP, "pointer", 1, "Test", "@THAT\nD=M\n@1\nD=D+A\n@R13\nM=D\n@SP\nM=M-1\nA=M\nD=M\n@R13\nA=M\nM=D\n"},
+		{C_PUSH, "static", 3, "Test", "@Test.3\nD=M\n@SP\nA=M\nM=D\n@SP\nM=M+1\n"},
+		{C_POP, "static", 3, "Test", "@SP\nM=M-1\nA=M\nD=M\n@Test.3\nM=D\n"},
+		{C_PUSH, "constant", 30, "Test", "@30\nD=A\n@SP\nA=M\nM=D\n@SP\nM=M+1\n"},
+	}
+	for _, test := range tests {
+		asmcommand, err := TranslatePushPop(test.ctype, test.seg, test.idx, test.fileName)
+		if err != nil {
+			t.Errorf("TranslatePushPop failed: %v", err)
+		}
+		if asmcommand != test.want {
+			var pushpop string
+			switch test.ctype {
+			case C_PUSH:
+				pushpop = "push"
+			case C_POP:
+				pushpop = "pop"
+			}
+			t.Errorf("TranslatePushPop(%q, %q, %d, %q) = %q, want %q", pushpop, test.seg, test.idx, test.fileName, asmcommand, test.want)
+		}
+	}
+}
+
+// TODO: Add tests for TranslateArithmetic
