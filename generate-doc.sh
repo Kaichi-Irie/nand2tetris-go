a705dc355f0bea9357a9d1517ccc9c8144fac000
Kaichi-Irie <jjsnoseitodesu@gmail.com>
2025-03-27 10:38:20 +0900

    :sparkles: Update Go documentation generation script and add HTML files for assembler and vm packages

--- generate-doc.sh
+++ generate-doc.sh
@@ -5,7 +5,6 @@ RUNNING_PID=$!      # godocのPIDを取得
 sleep 1             # godocの実行を1秒待機
 wget -np -k -p -q -r -E http://localhost:6060/pkg/nand2tetris-golang/?m=all
 kill ${RUNNING_PID} # godocの実行を終了
-find localhost:6060 -name "index.html?m=all.html" -delete
 rm -rf docs/lib/*
 rm -rf docs/pkg/*
 mv -f localhost:6060/lib docs
