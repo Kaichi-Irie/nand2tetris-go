643dc2d8b565cc057c041a559324439478469505
Kaichi-Irie <jjsnoseitodesu@gmail.com>
2025-03-27 11:11:22 +0900

    :zap: Update documentation generation script and improve package descriptions for clarity

--- generate-doc.sh
+++ generate-doc.sh
@@ -2,7 +2,7 @@
 
 godoc -http=:6060 & # バックグラウンド実行
 RUNNING_PID=$!      # godocのPIDを取得
-sleep 1             # godocの実行を1秒待機
+sleep 3            # godocの実行を3秒待機
 wget -np -k -p -q -r -E http://localhost:6060/pkg/nand2tetris-golang/?m=all
 kill ${RUNNING_PID} # godocの実行を終了
 rm -rf docs/lib/*
