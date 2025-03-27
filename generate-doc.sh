32a134c505f87c11ffca5658c5e1134ef55a7357
Kaichi-Irie <jjsnoseitodesu@gmail.com>
2025-03-27 11:38:36 +0900

    :zap: Add support for displaying internal function documentation in generated docs

--- generate-doc.sh
+++ generate-doc.sh
@@ -5,8 +5,11 @@ RUNNING_PID=$!      # godocのPIDを取得
 sleep 3            # godocの実行を3秒待機
 wget -np -k -p -q -r -E http://localhost:6060/pkg/nand2tetris-golang/?m=all
 kill ${RUNNING_PID} # godocの実行を終了
+find localhost:6060 -name "index.html" -delete
 rm -rf docs/lib/*
 rm -rf docs/pkg/*
 mv -f localhost:6060/lib docs
 mv -f localhost:6060/pkg docs
+# rename docs/pkg/nand2tetris-golang/index.html?m=all.html to docs/pkg/nand2tetris-golang/index.html
+mv -f docs/pkg/nand2tetris-golang/index.html?m=all.html docs/pkg/nand2tetris-golang/index.html
 rm -rf localhost:6060
