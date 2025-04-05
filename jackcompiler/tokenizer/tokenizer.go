9450689a86ea4802d62317bc4e70f08507d2dfcd
Kaichi-Irie <jjsnoseitodesu@gmail.com>
2025-04-05 10:47:27 +0900

    :sparkle: Add judging functions and extracting functions.

--- jackcompiler/tokenizer/tokenizer.go
+++ jackcompiler/tokenizer/tokenizer.go
@@ -97,21 +97,21 @@ func (t Tokenizer) advance() bool {
 	}
 
 	// check if the next token is an integer constant
-	if i, ok := extractIntConst(t.currentLine[pos:]); ok == nil {
+	if i, ok := ExtractIntConst(t.currentLine[pos:]); ok == nil {
 		t.currentPos += len(strconv.Itoa(i))
 		t.currentToken = strconv.Itoa(i)
 		return true
 	}
 
 	// check if the next token is a string constant
-	if s, ok := extractStringConst(t.currentLine[pos:]); ok == nil {
+	if s, ok := ExtractStringConst(t.currentLine[pos:]); ok == nil {
 		t.currentPos += len(s) + 2 // 2 for the quotes
 		t.currentToken = s
 		return true
 	}
 
 	// check if the next token is an identifier
-	if id, ok := extractIdentifier(t.currentLine[pos:]); ok == nil {
+	if id, ok := ExtractIdentifier(t.currentLine[pos:]); ok == nil {
 		t.currentPos += len(id)
 		t.currentToken = id
 		return true
@@ -119,42 +119,134 @@ func (t Tokenizer) advance() bool {
 	return false
 }
 
-// func getTokenType(token string) tokenType
-// func getKeyWordType(token string) keyWordType
-
 // func intVal(token string) (int, error)
 // func stringVal(token string) (string, error)
 
-// extractIntConst extracts the integer constant from the string
-func extractIntConst(s string) (int, error) {
-	i := strings.IndexAny(s, " \t\n\r(){}[];,.+-*/&|<>=~_abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ")
-	if i == -1 || i == 0 {
+// IsSymbol checks if the token is a symbol
+func IsSymbol(token string) bool {
+	for _, s := range symbols {
+		if token == s {
+			return true
+		}
+	}
+	return false
+}
+
+// IsKeyword checks if the token is a keyword
+func IsKeyword(token string) bool {
+	for _, kw := range keywords {
+		if token == kw {
+			return true
+		}
+	}
+	return false
+}
+
+/*
+IsIntConst checks if the string is an integer constant. This does not process any spaces, which means "123 " or "3\n" is not an integer constant
+and "123" is an integer constant.
+*/
+func IsIntConst(s string) bool {
+	_, err := strconv.Atoi(s)
+	return err == nil
+}
+
+/*
+ExtractIntConst extracts the integer constant at the beginning of the string.
+It returns the integer constant and an error if the string does not start with an integer constant.
+It also checks if the integer constant is a valid integer constant.
+*/
+func ExtractIntConst(s string) (int, error) {
+	// check if the string is integer constant by itself
+	if IsIntConst(s) {
 		return strconv.Atoi(s)
 	}
-	return strconv.Atoi(s[0:i])
+
+	// check if the string starts with an integer constant
+	i := strings.IndexAny(s, " \t\n\r(){}[];,.+-*/&|<>=~_abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ")
+	if i == -1 {
+		i = len(s)
+	}
+	intConst := s[0:i]
+	if !IsIntConst(intConst) {
+		return 0, fmt.Errorf("not an integer constant")
+	}
+	return strconv.Atoi(intConst)
 }
 
-// extractStringConst extracts the string constant from the string
-func extractStringConst(s string) (string, error) {
-	if s[0] != '"' {
-		return "", fmt.Errorf("not a string constant")
+// IsStringConst checks if the string is a string constant
+func IsStringConst(s string) bool {
+	if len(s) < 2 || s[0] != '"' || s[len(s)-1] != '"' {
+		return false
+	}
+	// check if the string contains any quotes or newlines inside
+	if idx := strings.IndexAny(s[1:len(s)-1], "\"\n\r"); idx != -1 {
+		return false
+	}
+	return true
+}
+
+/*
+extractStringConst extracts the string constant at the beginning of the string.
+It returns the string constant without the quotes and an error if the string is not a string constant.
+*/
+func ExtractStringConst(s string) (string, error) {
+	if len(s) == 0 {
+		return "", fmt.Errorf("empty string")
 	}
 	idx := strings.Index(s[1:], "\"")
-	if idx == -1 {
+	strConst := s[1 : 1+idx]
+
+	if !IsStringConst("\"" + strConst + "\"") {
 		return "", fmt.Errorf("not a string constant")
 	}
-	return s[1 : 1+idx], nil
+	return strConst, nil
 }
 
-// extractIdentifier extracts the identifier from the string. An identifier is a string that starts with a letter or an underscore
-func extractIdentifier(s string) (string, error) {
-	// check if the first character is a letter or an underscore
-	if s[0] != '_' && (s[0] < 'A' || s[0] > 'z') {
-		return "", fmt.Errorf("not an identifier")
+func IsIdentifier(s string) bool {
+	if len(s) == 0 {
+		return false
+	}
+	// check if the name does not conflict with a keyword
+	if IsKeyword(s) {
+		return false
+	}
+
+	// check if the rest of the string is a letter, digit or underscore
+	for i, c := range s {
+		// check if the character is a letter, digit or underscore
+		if 'A' <= c && c <= 'Z' {
+			continue
+		}
+		if 'a' <= c && c <= 'z' {
+			continue
+		}
+		if i != 0 && '0' <= c && c <= '9' {
+			// the first character cannot be a digit
+			// but the rest of the string can be a digit
+			continue
+		}
+		if c == '_' {
+			continue
+		}
+		return false
+	}
+	return true
+}
+
+// ExtractIdentifier extracts the identifier from the string. An identifier is a string that starts with a letter or an underscore
+func ExtractIdentifier(s string) (string, error) {
+	if IsIdentifier(s) {
+		return s, nil
 	}
 	idx := strings.IndexAny(s, " \t\n\r(){}[];,.+-*/&|<>=~")
 	if idx == -1 {
-		return s, nil
+		idx = len(s)
 	}
-	return s[0:idx], nil
+
+	identifier := s[0:idx]
+	if !IsIdentifier(identifier) {
+		return "", fmt.Errorf("not an identifier")
+	}
+	return identifier, nil
 }
--- jackcompiler/tokenizer/tokenizer_test.go
+++ jackcompiler/tokenizer/tokenizer_test.go
@@ -2,6 +2,27 @@ package tokenizer
 
 import "testing"
 
+func TestIsIntConst(t *testing.T) {
+	tests := []struct {
+		token string
+		want  bool
+	}{
+		{"123", true},
+		{"0", true},
+		{"999", true},
+		{"098", true},
+		{"098hello", false},
+		{"3+5", false},
+		{"3   ", false},
+	}
+	for _, tt := range tests {
+		t.Run(tt.token, func(t *testing.T) {
+			if got := IsIntConst(tt.token); got != tt.want {
+				t.Errorf("isIntConst(%s) = %v, want %v", tt.token, got, tt.want)
+			}
+		})
+	}
+}
 func TestExtractIntConst(t *testing.T) {
 	tests := []struct {
 		token string
@@ -18,7 +39,7 @@ func TestExtractIntConst(t *testing.T) {
 	}
 	for _, tt := range tests {
 		t.Run(tt.token, func(t *testing.T) {
-			if got, err := extractIntConst(tt.token); err != nil {
+			if got, err := ExtractIntConst(tt.token); err != nil {
 				t.Errorf("extractIntConst(%s) = %v, want %v", tt.token, err, tt.want)
 			} else if got != tt.want {
 				t.Errorf("extractIntConst(%s) = %v, want %v", tt.token, got, tt.want)
@@ -26,6 +47,28 @@ func TestExtractIntConst(t *testing.T) {
 		})
 	}
 }
+
+func TestIsStringConst(t *testing.T) {
+	tests := []struct {
+		token string
+		want  bool
+	}{
+		{"\"hello\"", true},
+		{"\"\"", true},
+		{"\"123\"", true},
+		{"\"hello world\"", true},
+		{"hello", false},
+		{"123", false},
+	}
+	for _, tt := range tests {
+		t.Run(tt.token, func(t *testing.T) {
+			if got := IsStringConst(tt.token); got != tt.want {
+				t.Errorf("isStringConst(%s) = %v, want %v", tt.token, got, tt.want)
+			}
+		})
+	}
+}
+
 func TestExtractStringConst(t *testing.T) {
 	tests := []struct {
 		token string
@@ -38,10 +81,73 @@ func TestExtractStringConst(t *testing.T) {
 	}
 	for _, tt := range tests {
 		t.Run(tt.token, func(t *testing.T) {
-			if got, err := extractStringConst(tt.token); err != nil {
+			if got, err := ExtractStringConst(tt.token); err != nil {
 				t.Errorf("extractStringConst(%s) = %v, want %v", tt.token, err, tt.want)
 			} else if got != tt.want {
 				t.Errorf("extractStringConst(%s) = %v, want %v", tt.token, got, tt.want)
+			}
+		})
+	}
+}
+
+func TestIsIdentifier(t *testing.T) {
+	tests := []struct {
+		token string
+		want  bool
+	}{
+		{"hello", true},
+		{"_hello", true},
+		{"hello_world", true},
+		{"hello123", true},
+		{"3hello", false},
+		{" hello", false},
+		{"hello ", false},
+		{"hello+world", false},
+		{"hello-world", false},
+		{"hello.world", false},
+		{"hello,world", false},
+		{"hello;world", false},
+		{"hello:world", false},
+		{"hello?world", false},
+		{"hello!world", false},
+		{"hello@world", false},
+		{"hello#world", false},
+		{"hello$world", false},
+		{"hello%world", false},
+		{"hello^world", false},
+		{"hello&world", false},
+		{"hello*world", false},
+		{"hello(world", false},
+		{"hello)world", false},
+		{"class", false},
+		{"method", false},
+		{"function", false},
+		{"constructor", false},
+		{"int", false},
+		{"boolean", false},
+		{"char", false},
+		{"void", false},
+		{"var", false},
+		{"static", false},
+		{"field", false},
+		{"let", false},
+		{"do", false},
+		{"if", false},
+		{"else", false},
+		{"while", false},
+		{"return", false},
+		{"true", false},
+		{"false", false},
+		{"null", false},
+		{"this", false},
+		{"123", false},
+		{"\"hello\"", false},
+		{"\"hello world\"", false},
+		{"(", false}}
+	for _, tt := range tests {
+		t.Run(tt.token, func(t *testing.T) {
+			if got := IsIdentifier(tt.token); got != tt.want {
+				t.Errorf("isIdentifier(%s) = %v, want %v", tt.token, got, tt.want)
 			}
 		})
 	}
@@ -61,7 +167,7 @@ func TestExtractIdentifier(t *testing.T) {
 	}
 	for _, tt := range tests {
 		t.Run(tt.token, func(t *testing.T) {
-			if got, err := extractIdentifier(tt.token); err != nil {
+			if got, err := ExtractIdentifier(tt.token); err != nil {
 				t.Errorf("extractIdentifier(%s) = %v, want %v", tt.token, err, tt.want)
 			} else if got != tt.want {
 				t.Errorf("extractIdentifier(%s) = %v, want %v", tt.token, got, tt.want)
